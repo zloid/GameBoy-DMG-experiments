aa2c10d390cf0a761bbba9d59fac8a0652e52402
zloid <zloid-1@yandex.by>
2020-01-10 11:33:51 +0100

    sp meaning in stack

--- header.asm
+++ header.asm
@@ -1,6 +1,6 @@
-;-------------
-;	Header
-;-------------
+;================================================================
+;Header
+;================================================================
 
 SECTION "Copy Data",ROM0[$28] ;copy DMA routine to HRAM
 COPY_DATA:
@@ -52,8 +52,8 @@ SECTION "Start",ROM0[$100]
 	DB  $00,$08,$11,$1F,$88,$89,$00,$0E,$DC,$CC,$6E,$E6,$DD,$DD,$D9,$99
 	DB  $BB,$BB,$67,$63,$6E,$0E,$EC,$CC,$DD,$DC,$99,$9F,$BB,$B9,$33,$3E
 
-	; $0134-$013E Game title
-	DB	"PlantBoy   "
+	; $0134-$013E Game title - 11 char
+	DB	"idolzBoy   "
 		;0123456789A
 
 	; $013F-$0142 Product code, leave blank
