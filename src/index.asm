374f33a9172c01d17c096a4afc932051a457cee9
zloid <zloid-1@yandex.by>
2020-03-08 13:54:28 +0100

    refactoring

--- src/index.asm
+++ src/index.asm
@@ -2,12 +2,17 @@
 
 INCLUDE "features/boilerplate/constants/hardware.inc"
 INCLUDE "features/boilerplate/header/header.asm"
-INCLUDE "features/boilerplate/tiles/tiles.asm"
-INCLUDE "features/boilerplate/map/map.asm"
-INCLUDE "features/boilerplate/index.asm"
+; INCLUDE "features/boilerplate/tiles/tiles.asm"
+; INCLUDE "features/boilerplate/map/map.asm"
+; INCLUDE "features/boilerplate/index.asm"
 
-; ==============feature #2==============
+; ==============ram_oam_vars SECTION==============
+INCLUDE "features/boilerplate/head.asm"
 
-
-
+MAIN_GAME_LOOP:
+	call WAIT_VBLANK
+    
+	 
+	jp MAIN_GAME_LOOP
   
+INCLUDE "features/boilerplate/define_and_ram_oam_vars.asm"
